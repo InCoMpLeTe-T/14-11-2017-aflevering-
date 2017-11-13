57a6422b6f532825d9b84705417ac7be7f82140b
HOTSKPLAP036\bruger <mlc4447@hotmail.com>
2017-11-13 14:09:49 +0100

    spillet

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp4
+{
+    class gangstergame
+
+    {
+        static void Main(string[] args)
+        {
+           
+            string Saythx = "";
+
+            int milliseconds = 3000;
+            Thread.Sleep(milliseconds);
+            Textcontainer.Starttxt();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+
+
+            string Typename = " ";
+            Console.WriteLine("\r\n" + "hey manno Whats your name ?");
+            Typename = Console.ReadLine();
+            Console.WriteLine("okay " + Typename + " Welcome to the crew!");
+            Thread.Sleep(milliseconds);
+            Console.WriteLine("First of all we need to set you up with a piece");
+            Thread.Sleep(milliseconds);
+            Console.WriteLine("Here you go man!");
+            Textcontainer.Gun1();
+
+
+            while (true)
+            {
+
+
+                Saythx = Console.ReadLine();
+                if (Saythx != "Thanks")
+                {
+
+                    Console.WriteLine("Don't be dissrespectfull! and say ((Thanks)) and take it! ");
+
+                }
+                else
+                {
+                    Console.WriteLine("You are Welcome e'se");
+                    Thread.Sleep(milliseconds);
+
+
+                    break;
+                }
+
+            }
+            Console.WriteLine("Mission 1");
+            Thread.Sleep(milliseconds);
+            Console.WriteLine("okay manno.now take the car and go prove yourself!");
+            Thread.Sleep(milliseconds);
+            Console.WriteLine("up at 9'th there is a store run by Niggers!");
+            Console.WriteLine("thats mexican turf!");
+            Thread.Sleep(milliseconds);
+            Console.WriteLine("there's a door on the back of the building go see if you");
+            Console.WriteLine("can figure out the code. we might need that later! ");
+            Thread.Sleep(milliseconds);
+            Console.WriteLine("so take your shitty car and DO it!");
+            Thread.Sleep(milliseconds);
+            Mission1();
+
+        }
+        public static void Mission1()
+        {
+            string missionone = "";
+            int milliseconds = 5000;
+            Thread.Sleep(milliseconds);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+            //Mission one!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Textcontainer.Mission1();
+            Thread.Sleep(milliseconds);
+            Textcontainer.Mission1b();
+            Thread.Sleep(milliseconds);
+            Textcontainer.Mission1y();
+            Thread.Sleep(milliseconds);
+            Textcontainer.Mission1w();
+            Console.WriteLine(" witch one is it (Nr 1, Nr 2 or Nr 3) ese" );
+
+
+            while (true)
+            {
+
+
+                missionone = Console.ReadLine();
+                if (missionone != "Nr 1")
+                {
+                    
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("How stupid are you! Thats Not The NIgger Store!!");
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Finnaly! now we need to go back and find the code box! and guess the code! ");
+                    Thread.Sleep(milliseconds);
+
+                    Console.ReadLine();
+                    break;
+
+                }
+
+
+            }
+            Textcontainer.Mission1code();
+            Textcontainer.Mission1codelock();
+            while (true)
+            {
+
+
+                missionone = Console.ReadLine();
+                if (missionone != "8741")
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Wrong Code Carnal! Quickly Before the Mayate's Come!");
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Good ese, now remember the code (8741), W'll Come back later at Teach them a leason! ");
+                    Thread.Sleep(milliseconds);
+
+                    Console.ReadLine();
+                    break;
+
+                }
+
+
+            }
+            Textcontainer.Thedrive();
+            Thread.Sleep(milliseconds);
+            Textcontainer.Enterdinner();
+            Thread.Sleep(milliseconds);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Come on ese! make up your mind! ");
+            Console.ReadLine();
+            while (true)
+            {
+
+                
+                missionone = Console.ReadLine();
+                if (missionone != "Start shooting")
+                {
+                    
+                    Textcontainer.Thesetup();
+                    
+                    Textcontainer.Sluttxt();
+
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Mission 1 Complete! You have proven yourself to be a gagster!");
+                    Console.WriteLine("You won the game");
+
+
+                }
+
+
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/textcontainer.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Textcontainer
+    {
+        public static void Starttxt()
+
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("    ________________________________________      ");
+            Console.WriteLine("   |               Welcome to               |     ");
+            Console.WriteLine("   |           The GanGster Game            |     ");
+            Console.WriteLine("   | Will you survive a life on the street? |     ");
+            Console.WriteLine("   |            Let's Find Out!             |     ");
+            Console.WriteLine("   |________________________________________|     ");
+
+        }
+        public static void Sluttxt()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                   _______            ");
+            Console.WriteLine("                  |       |           ");
+            Console.WriteLine("                  |       |           ");
+            Console.WriteLine("         _________|       |_________  ");
+            Console.WriteLine("        |                           | ");
+            Console.WriteLine("        |       You Dead Mofo       | ");
+            Console.WriteLine("        |_________         _________| ");
+            Console.WriteLine("                  |  Game |          ");
+            Console.WriteLine("                  |  Over |          ");
+            Console.WriteLine("                  |  MAN! |          ");
+            Console.WriteLine("                  |       |          ");
+            Console.WriteLine("                  |       |          ");
+            Console.WriteLine("                  |       |          ");
+            Console.WriteLine("                  |_______|          ");
+
+        }
+        public static void Gun1()
+        {
+            Console.WriteLine("   £_______  ");
+            Console.WriteLine("  / __.==--' ");
+            Console.WriteLine(" /#(-'       ");
+            Console.WriteLine(" `-'         ");
+        }
+        public static void Mission1()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("    ________________________________________      ");
+            Console.WriteLine("   |          Around an Hour later          |     ");
+            Console.WriteLine("   |    (Cuz you Realy have a shitty car)   |     ");
+            Console.WriteLine("   |       You arive at 9'th street         |     ");
+            Console.WriteLine("   |   at finds 3 stores on the street!?    |     ");
+            Console.WriteLine("   |          Wich one is it ??             |     ");
+            Console.WriteLine("   |________________________________________|     ");
+        }
+        public static void Mission1b()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("    ________________________________________      ");
+            Console.WriteLine("   |                    Nr 1                |     ");
+            Console.WriteLine("   |             You can se there is        |     ");
+            Console.WriteLine("   |            a bunch of Mayate's         |     ");
+            Console.WriteLine("   |            in the store working!       |     ");
+            Console.WriteLine("   |              could it be there         |     ");
+            Console.WriteLine("   |________________________________________|     ");
+        }
+        public static void Mission1y()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("    ________________________________________      ");
+            Console.WriteLine("   |                    Nr 2                |     ");
+            Console.WriteLine("   |             You can se there is        |     ");
+            Console.WriteLine("   |             a bunch of chino's         |     ");
+            Console.WriteLine("   |            in the store working!       |     ");
+            Console.WriteLine("   |              could it be there?        |     ");
+            Console.WriteLine("   |________________________________________|     ");
+        }
+        public static void Mission1w()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("    ________________________________________      ");
+            Console.WriteLine("   |                    Nr 3                |     ");
+            Console.WriteLine("   |             You can se there is        |     ");
+            Console.WriteLine("   |             a bunch of Guero's         |     ");
+            Console.WriteLine("   |            in the store working!       |     ");
+            Console.WriteLine("   |              could it be there?        |     ");
+            Console.WriteLine("   |________________________________________|     ");
+        }
+        public static void Mission1code()
+        {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("    ________________________________________      ");
+                Console.WriteLine("   |            You get to the              |     ");
+                Console.WriteLine("   |          back of the store             |     ");
+                Console.WriteLine("   |   And Find the Door With Codelock      |     ");
+                Console.WriteLine("   |     And see there is Greecy Finger on  |     ");
+                Console.WriteLine("   |                                        |     ");
+                Console.WriteLine("   |                 4781                   |     ");
+                Console.WriteLine("   |________________________________________|     ");
+        }
+        public static void Mission1codelock()
+        {
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("             _____________                        ");
+                Console.WriteLine("            |             |                       ");
+                Console.WriteLine("            | |7| |8| |9| |                       ");
+                Console.WriteLine("            |             |                       ");
+                Console.WriteLine("            | |4| |5| |6| |                       ");
+                Console.WriteLine("            |             |                       ");
+                Console.WriteLine("            | |1| |2| |3| |                       ");
+                Console.WriteLine("            |_____________|                       ");
+            
+        }
+        public static void Thedrive()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        White                            _________  ");
+            Console.WriteLine("             _,---.                          _..--'/         \\");
+            Console.WriteLine("          ,-'      )                    _,-,'     /           \\");
+            Console.WriteLine("         (          )               _,-'  /      /             \\");
+            Console.WriteLine("          `-.__, -'             _,-/     /     _/ ,------------.\\");
+            Console.WriteLine("     ,--._,---.             _,-'  /     /_,--'' | |  B-News    | |");
+            Console.WriteLine("   _(_,-'      )        _,-'/    /_, - '|       | |  Gangcrime | |");
+            Console.WriteLine(" ,'  (        )      ,'/   /_, -'|      ||_  -  | |     is     | |");
+            Console.WriteLine("(     `--._,-'    _,' /_,-'|     ||_  - |       | |  Sky High  | |");
+            Console.WriteLine(" `--.__,-'     _,'/,-'|  - ||_ - |      ||_  -  | |            | |");
+            Console.WriteLine("            _,'/,'|  - ___ |     ||_  - |       | `------------' |");
+            Console.WriteLine("        _ ,' |_' -|- _(   ) |_   |      ||_  -  |                |");
+            Console.WriteLine("       ( ): ((`) -| (. `-/ )   - ||_  - |       |                |");
+            Console.WriteLine("      ` | '(-.,') '( \\`/-') _ - |      | _   - |                |");
+            Console.WriteLine("        `   (_.) -(_._\\|,-_)    ||_  - |       |                |");
+            Console.WriteLine(" .        `. || ` | (\\||_)  _ - |      ||_  -  |                |");
+            Console.WriteLine("             `.    ` . ||  |   - ||_  - |       |                |");
+            Console.WriteLine("                `.     ||-.|  :  |  _   ||_   - |     __________ |");
+            Console.WriteLine("                   `.  ||    ` -.|   |  |   _   |    | _Diener_ |`'--.._");
+            Console.WriteLine("                      `.           ` - .|  | |  |    || ()._o  ||   __ |");
+            Console.WriteLine("\\           `            `.   `           ` -'.|____|`'---.|>_||  |. ||");
+            Console.WriteLine("                            `.                         `' -- .._|__|__||");
+            Console.WriteLine("                      ,----.---------- .                            .-_.");
+            Console.WriteLine("   _                 |.`.,' `.           `.                         || |");
+            Console.WriteLine("   \\               \\ _|`. / `.            `.                     \\|_o");
+            Console.WriteLine("    \\                |\\  `.  \\.-----_-----.\\     `");
+            Console.WriteLine("     \\               `.|.`| `./ \\  c__)___/ \\             .");
+            Console.WriteLine("      \\        `         `.   `. \\___\\___)__\\");
+            Console.WriteLine("                            `. _ `\\`___________\\");
+            Console.WriteLine("                              |\\  |_|   ____   |_|");
+            Console.WriteLine("          _                   `.|\\|#|__[MEXi]__|_|`.");
+            Console.WriteLine("          \\                      `================'  `.");
+
+
+
+
+        }
+
+        public static void Enterdinner()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("    ________________________________________      ");
+            Console.WriteLine("   |You get back to the store, several later|     ");
+            Console.WriteLine("   |and enter the code. its seems that the  |     ");
+            Console.WriteLine("   |place is empty and all the lights is    |     ");
+            Console.WriteLine("   |turned off. you go to turn on the lights|     ");
+            Console.WriteLine("   |and realise it's an ambush! and the     |     ");
+            Console.WriteLine("   | rivel gang now has a gun to your crew  |     ");
+            Console.WriteLine("   | leders head! what do you do!           |     ");
+            Console.WriteLine("   | 1. Hand over my piece                  |     ");
+            Console.WriteLine("   | 2. Start shooting                      |     ");
+            Console.WriteLine("   |________________________________________|     ");
+
+        }
+
+        public static void Thesetup()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                         .;;;;;;;;;.                           ");
+             Console.WriteLine("                         ;;;;;;;;;;;;; / /                    ");
+             Console.WriteLine("                        ; ; '    ;;;; / /                     ");
+             Console.WriteLine("   £_______  _________ ;       _    ;_____________ ->         ");
+             Console.WriteLine("  / __.==--'         n; ___`^/ ___,; n \\ \\                  ");
+             Console.WriteLine(" /#(-'               ) | ' -'` '`- `|(   \\ \\                ");
+             Console.WriteLine(" `-'                  `(    ;.     )'                         ");
+             Console.WriteLine("                        \\ /``-'\\ ' /                        ");
+             Console.WriteLine("                         ||`_____ |                           ");
+             Console.WriteLine("                         `.',---  ,'                          ");
+             Console.WriteLine("                          \\  ^  /                            ");
+             Console.WriteLine("                            `---'                             ");
+        }
+           
+
+    }
+}
